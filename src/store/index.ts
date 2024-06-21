72197ce829c933e55dde2d1a46ebdd8a1a887984
Iurii Kyrylovskyi <iurii712@gmail.com>
2024-06-21 12:12:25 +0300

    refactor store state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,30 +1,40 @@
 import defineLimits from '@/helpers/defineLimits';
 import { INavItem } from '@/interfaces/navItems';
 import navItems from '@/utils/navItems';
-import { frequencyGap } from 'src/utils/constants';
+import { frequencyGap, minFrequency } from 'src/utils/constants';
 import { InjectionKey } from 'vue';
 import { createStore, useStore as baseUseStore, Store } from 'vuex';
 
 export const key: InjectionKey<Store<INavItem[]>> = Symbol();
-export const store = createStore<INavItem[]>({
-  state: navItems,
+interface IStoreState {
+  entered: number;
+  navItems: INavItem[];
+}
+
+const initState: IStoreState = {
+  entered: minFrequency,
+  navItems: navItems,
+};
+
+export const store = createStore<IStoreState>({
+  state: initState,
   mutations: {
-    increaseFrequency(state: INavItem[]) {
-      return state.map((el) =>
+    increaseFrequency(state: IStoreState) {
+      return state.navItems.map((el) =>
         el.isActive && defineLimits(el.frequency, 'asc')
           ? (el.frequency += frequencyGap)
           : el
       );
     },
-    decreaseFrequency(state: INavItem[]) {
-      return state.map((el) =>
+    decreaseFrequency(state: IStoreState) {
+      return state.navItems.map((el) =>
         el.isActive && defineLimits(el.frequency, 'des')
           ? (el.frequency -= frequencyGap)
           : el
       );
     },
-    toggleActiveFreq(state: INavItem[]) {
-      return state.map((el) =>
+    toggleActiveFreq(state: IStoreState) {
+      return state.navItems.map((el) =>
         el.isActive ? (el.isActive = false) : (el.isActive = true)
       );
     },
